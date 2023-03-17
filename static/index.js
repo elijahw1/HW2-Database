3ef974f4f307d9a52725fb4d4d44cdfba039d24e
elijahw1 <76186976+elijahw1@users.noreply.github.com>
2023-03-17 13:59:41 -0400

    Delete static directory

--- static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteUser(userId){
-    fetch('/delete-user', {
-        method: "POST",
-        body: JSON.stringify({ userId: userId}),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
